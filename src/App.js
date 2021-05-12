417dcc121bc2b0da016a8db3c2ae2f5e44aae186
hyeongjuSung <tjdgudwn1@gmail.com>
2021-05-12 12:43:29 +0900

    material.ui 연결 및 datetime picker 설정

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { TextField, Typography } from '@material-ui/core';
+import { KeyboardDatePicker, KeyboardTimePicker } from '@material-ui/pickers';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          react
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      content: "",
+      startDate: null,
+      startTime: null,
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="header">TODO LIST</div>
+        <div className="input_area">
+          <TextField label="제목" size="normal" margin="normal" fullWidth required />
+          <TextField label="상세내용" size="normal" margin="normal" fullWidth multiline />
+          <KeyboardDatePicker
+            disableToolbar
+            variant="inline"
+            format="yyyy/MM/DD"
+            margin="normal"
+            label="시작 예정일"
+            onChange={(value)=>console.log(value)} 
+            style = {{width: '50%'}}     
+            KeyboardButtonProps={{
+              'aria-label': 'change date',
+            }}
+          />
+          <KeyboardTimePicker
+            margin="normal"
+            label="시작시간"
+            variant="inline"
+            onChange={(value)=>console.log(value)}   
+            style = {{width: '50%'}}   
+            KeyboardButtonProps={{
+              'aria-label': 'change time',
+            }}
+          />
+        </div>
+        <div className="list_area">리스트 영역</div>
+        <Typography variant="body2" color="textSecondary" align="center">
+          {'Copyright © 성형주 '+new Date().getFullYear()+'.'}         
+        </Typography>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
